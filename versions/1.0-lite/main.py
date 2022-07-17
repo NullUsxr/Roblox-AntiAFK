be0f7eedc45d546092d6fcbffb2786a4894d7a55
NullUsxr <nullusxr@tuta.io>
2022-07-17 16:07:45 -0700

    Added lite version that is not OS dependent

---
+++ versions/1.0-lite/main.py
@@ -0,0 +1,14 @@
+# NullUsxr 2020 | Roblox AntiAFK program
+import time
+import pyautogui
+print("Starting...")
+time.sleep(5)
+print("[INFO] AntiAFK is now active")
+while 1 == 1:
+    time.sleep(1000)
+    print("[ALERT] Please make Roblox the active window")
+    time.sleep(20)
+    pyautogui.keyDown('LEFT')
+    time.sleep(1)
+    pyautogui.keyUp('LEFT')
+    print("[INFO] Completed a cycle")
